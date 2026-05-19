7ffab30e86f5524a89c2bba09b21d965dc2c6953
Milen kouylekov <kouylekov@gmail.com>
2026-05-19 10:30:24 +0200

    Link entire hyprland directory

--- common/home.nix
+++ common/home.nix
@@ -12,9 +12,9 @@
     "alacritty/alacritty.toml".source = link "alacritty/alacritty.toml";
     "ghostty/config".source = link "ghostty/config";
     "fuzzel/fuzzel.ini".source = link "fuzzel/fuzzel.ini";
-    "hypr/hyprland.lua".source = link "hypr/hyprland.lua";
-    "hypr/hypridle.conf".source = link "hypr/hypridle.conf";
-    "hypr/hyprlock.conf".source = link "hypr/hyprlock.conf";
+    "hypr" = {
+      source = link "hypr";
+    };
     "mako/config".source = link "mako/config";
     "waybar/config".source = link "waybar/config";
     "waybar/style.css".source = link "waybar/style.css";
